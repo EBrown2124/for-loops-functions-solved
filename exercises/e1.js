011e3384ef120430eb8dbd15518109ee7b824fbe
EBrown2124 <110208836+EBrown2124@users.noreply.github.com>
2023-04-09 22:11:41 -0700

    passed

--- exercises/e1.js
+++ exercises/e1.js
@@ -7,7 +7,7 @@ export function getFirstTenNumbers() {
   // Your code goes here...
   var numArray = [1,2,3,4,5,6,7,8,9,10];
 
-  return numArray; tim
+  return numArray;
 }
 
 console.log(getFirstTenNumbers())
